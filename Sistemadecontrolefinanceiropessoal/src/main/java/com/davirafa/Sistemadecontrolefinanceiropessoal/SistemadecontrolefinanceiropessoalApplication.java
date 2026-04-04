6b48381bc6b19b1eb0f677e28269203a67fdc13a
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-03 21:59:49 -0300

    primeiro commit

---
+++ Sistemadecontrolefinanceiropessoal/src/main/java/com/davirafa/Sistemadecontrolefinanceiropessoal/SistemadecontrolefinanceiropessoalApplication.java
@@ -0,0 +1,13 @@
+package com.davirafa.Sistemadecontrolefinanceiropessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadecontrolefinanceiropessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadecontrolefinanceiropessoalApplication.class, args);
+	}
+
+}
---
+++ Sistemadecontrolefinanceiropessoal/src/test/java/com/davirafa/Sistemadecontrolefinanceiropessoal/SistemadecontrolefinanceiropessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davirafa.Sistemadecontrolefinanceiropessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadecontrolefinanceiropessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
