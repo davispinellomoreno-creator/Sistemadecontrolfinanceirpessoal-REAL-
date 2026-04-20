178cff65fe891c16552a46178103349913c0d5c6
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-19 21:41:39 -0300

    Começando o service

--- Sistemadecontrolefinanceiropessoal/src/main/java/Model/Entity.java
+++ Sistemadecontrolefinanceiropessoal/src/main/java/Model/Entity.java
@@ -1,6 +1,7 @@
 package Model;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,7 @@ import java.time.LocalDate;
 public class Entity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private long id;
    private String nomeDoGasto;
     private LocalDate dia;
--- Sistemadecontrolefinanceiropessoal/src/main/java/Service/Service.java
+++ Sistemadecontrolefinanceiropessoal/src/main/java/Service/Service.java
@@ -1,7 +1,37 @@
 package Service;
 
+import Model.Entity;
+import Repository.Repository;
+
+
 @org.springframework.stereotype.Service
 
 
 public class Service {
-}
+
+
+    private final Repository repository;
+ private final Entity entity;
+
+   public Service(Repository repository, Entity entity) {
+        this.repository = repository;
+       this.entity = entity;
+   }
+
+    public void salvarGasto(Entity gasto) {
+        repository.saveAndFlush(entity);
+    }
+
+    public void buscarGasto(Long id) throws Throwable {
+        repository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Usuário não encontrado"));
+    }
+
+       public void deletarGasto(Long id){
+          repository.deleteById(id);
+
+        }
+
+   }
+
