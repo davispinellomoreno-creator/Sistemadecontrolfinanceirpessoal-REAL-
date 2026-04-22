41ce7b040b2d95b32a84d08559ad699c63b6e415
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-22 10:39:11 -0300

    fazendo um pequeno ajuste, no repositorio

--- Sistemadecontrolefinanceiropessoal/src/main/java/Repository/Repository.java
+++ Sistemadecontrolefinanceiropessoal/src/main/java/Repository/Repository.java
@@ -1,6 +1,7 @@
 package Repository;
 
+import Model.Gastos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Repository extends JpaRepository {
+public interface Repository extends JpaRepository<Gastos, Long> {
 }
